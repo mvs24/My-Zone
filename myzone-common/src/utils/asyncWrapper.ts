80e27676ef1eae766ebe33fafd0ee78406483f93
mvs24 <Marius.Vasili@fti.edu.al>
2021-09-23 21:18:09 +0200

    Updates

--- myzone-common/src/utils/asyncWrapper.ts
+++ myzone-common/src/utils/asyncWrapper.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 export const asyncWrapper: (
   fn: Function
-) => (req: Request, res: Response, next: NextFunction) => void =
-  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next);
-  };
+) => (req: Request, res: Response, next: NextFunction) => void = (
+  fn: Function
+) => (req: Request, res: Response, next: NextFunction) => {
+  fn(req, res, next).catch(next);
+};
